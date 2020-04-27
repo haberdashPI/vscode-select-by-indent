c62ecc74cd050a402bd8e0777d86082004ad0e08
haberdashpi <david.frank.little@gmail.com>
2020-04-27 14:53:35 -0400

    wip: indentation selection

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {Range, Position} from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,6 +20,44 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(disposable);
 }
 
+function lineRange(doc: vscode.TextDocument, from: number,to: number){
+    return doc.lineCount > to ?
+        new Range(new Position(from,0), new Position(to+1,0)) :
+        new Range(new Position(from,0), doc.lineAt(to).range.end)
+}
+
+function findIndents(doc: vscode.TextDocument, text: string){
+    let lines = text.split(/[\r\n]+/)
+
+    let tabSizeSetting = vscode.workspace.getConfiguration('editor',doc).
+        get<number>('tabSize')
+    let tabSize = tabSizeSetting ? tabSizeSetting : 4;
+    let tab = '';
+    for(let i=0;i<tabSize;i++){
+        tab += ' ';
+    }
+
+    return lines.map(line => {
+        if(line.match(/^\s+$/)){
+            return undefined
+        }else{
+            let indent = line.match(/^\s*/)
+            if(indent){
+                let indentStr = indent[0].replace(/\t/,tab)
+                return indentStr.length
+            }
+            return 0;
+        }
+    })
+}
+
+function minUndef(x: number | undefined, y: number | undefined){
+    if(x === undefined && y === undefined) return undefined
+    else if(x === undefined) return y
+    else if(y == undefined) return x
+    else return Math.min(x,y)
+}
+
 function expandByIndent(editor: vscode.TextEditor){
     return function(sel: vscode.Selection){
         // TODO: plan, check if the outer indent is selected
@@ -27,17 +66,27 @@ function expandByIndent(editor: vscode.TextEditor){
         // select all lines above and below with the same
         // indent (if they exist) or, one indent out
         let doc = editor.document;
-        let lines = getLines(doc,sel);
-        let indents = lines.map(findIndent,lines);
-        let minIndent = indents.reduce(Math.min)
-        let unselectedOuter = indents.length > 2 &&
-            indents[0] === indents[indents.length-1] &&
-            indents[0] === minIndent && indents[0] !== indents[1] &&
-            indents[indents.length-1] !== indents[indents.length-2]
 
-        if(outerSelected)
 
+        let from = sel.start.line
+        let to = sel.end.line
+        let lines = doc.getText(lineRange(doc,from,to));
+        let indents = findIndents(doc,lines);
+        let minIndent = indents.reduce(minUndef)
+        let before = findIndents(doc,doc.getText(lineRange(doc,from-1,from-1)))
+        let after = findIndents(doc,doc.getText(lineRange(doc,to+1,to+1)))
+
+        let unselectedOuter = (after ? after < minIndent : true) &&
+            (before ? before < minIndent && before == after : false)
+
+        if(unselectedOuter){
+            return new vscode.Selection(lineRange(doc,from-1,to+1))
+        }else{
+            let start = findIndentBefore(doc,minIndent,from)
+            let end = findIndentAfter(doc,minIndent,to)
+            return new vscode.Selection(lineRange(doc,start,end))
+        }
     }
-
+}
 // this method is called when your extension is deactivated
 export function deactivate() {}
