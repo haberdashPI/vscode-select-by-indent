0ceefa22a34e5015cbf45d4226e428388cc4e0c6
haberdashpi <david.frank.little@gmail.com>
2020-04-28 12:14:24 -0400

    wip: selection

--- src/extension.ts
+++ src/extension.ts
@@ -6,12 +6,11 @@ import {Range, Position} from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('vscode-move-by-indent.select', () => {
-        let editor = vscode.window.activeTextEditor();
+        let editor = vscode.window.activeTextEditor;
         if(editor){
             editor.selections = editor.selections.map(expandByIndent(editor));
         }
@@ -38,7 +37,7 @@ function findIndents(doc: vscode.TextDocument, text: string){
     }
 
     return lines.map(line => {
-        if(line.match(/^\s+$/)){
+        if(line.length === 0 || line.match(/^\s+$/)){
             return undefined
         }else{
             let indent = line.match(/^\s*/)
@@ -51,6 +50,14 @@ function findIndents(doc: vscode.TextDocument, text: string){
     })
 }
 
+function findIndent(doc: vscode.TextDocument, text: string){
+    let indents = findIndents(doc, text)
+    if(indents.length !== 1){
+        vscode.window.showErrorMessage("Expected single line")
+    }
+    return indents[0];
+}
+
 function minUndef(x: number | undefined, y: number | undefined){
     if(x === undefined && y === undefined) return undefined
     else if(x === undefined) return y
@@ -58,34 +65,53 @@ function minUndef(x: number | undefined, y: number | undefined){
     else return Math.min(x,y)
 }
 
+function findIndentBefore(doc: vscode.TextDocument,minIndent: number,from: number){
+    let at = from-1;
+    while(at > 0){
+        let indent = findIndent(doc,doc.getText(lineRange(doc,at,at)))
+        if(indent !== undefined && indent < minIndent) return at+1
+        at--;
+    }
+    return 0;
+}
+
+function findIndentAfter(doc: vscode.TextDocument,minIndent: number,to: number){
+    let at = to+1;
+    while(at <= doc.lineCount){
+        let indent = findIndent(doc,doc.getText(lineRange(doc,at,at)))
+        if(indent !== undefined && indent < minIndent) return at-1
+        at++;
+    }
+    return doc.lineCount;
+}
+
 function expandByIndent(editor: vscode.TextEditor){
     return function(sel: vscode.Selection){
-        // TODO: plan, check if the outer indent is selected
-        // (line above and (possibly below) one indent out)
-        // and select just those lines, otherweise
-        // select all lines above and below with the same
-        // indent (if they exist) or, one indent out
         let doc = editor.document;
-
 
         let from = sel.start.line
         let to = sel.end.line
         let lines = doc.getText(lineRange(doc,from,to));
         let indents = findIndents(doc,lines);
         let minIndent = indents.reduce(minUndef)
-        let before = findIndents(doc,doc.getText(lineRange(doc,from-1,from-1)))
-        let after = findIndents(doc,doc.getText(lineRange(doc,to+1,to+1)))
+        if(minIndent){
+            let before = findIndent(doc,doc.getText(lineRange(doc,from-1,from-1)))
+            let after = findIndent(doc,doc.getText(lineRange(doc,to+1,to+1)))
 
-        let unselectedOuter = (after ? after < minIndent : true) &&
-            (before ? before < minIndent && before == after : false)
+            let unselectedOuter = (after ? after < minIndent : true) &&
+                (before ? before < minIndent && before === after : false)
 
-        if(unselectedOuter){
-            return new vscode.Selection(lineRange(doc,from-1,to+1))
-        }else{
-            let start = findIndentBefore(doc,minIndent,from)
-            let end = findIndentAfter(doc,minIndent,to)
-            return new vscode.Selection(lineRange(doc,start,end))
+            if(unselectedOuter){
+                let range = lineRange(doc,from-1,to+1)
+                return new vscode.Selection(range.start,range.end)
+            }else{
+                let start = findIndentBefore(doc,minIndent,from)
+                let end = findIndentAfter(doc,minIndent,to)
+                let range = lineRange(doc,start,end)
+                return new vscode.Selection(range.start,range.end)
+            }
         }
+        return sel
     }
 }
 // this method is called when your extension is deactivated
